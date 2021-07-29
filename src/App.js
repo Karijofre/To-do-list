099dab983d3f24ba0f0c30e3cf8e300fa29587da
karijofre <karinajofre@hotmail.com>
2021-07-29 18:27:52 +0200

    subiendo lista de tareas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
 
 
 
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div>
       <header>
-      <h1> To Do List 📑</h1>
+      <h1> To Do List 📑 </h1>
       </header>
-      <Form />
+      <Form 
+        inputText = {inputText}
+        todos = {todos}
+        setTodos = {setTodos}
+        setInputText= {setInputText}
+      />
+      <TodoList 
+        todos={todos}
+      />
     </div>
   );
 }
